86f12f2de4c47a4730e0e1ac9149cc2d278198b8
Pietsch, Martin <martin.pietsch@tu-dresden.de>
2022-12-29 15:07:26 +0100

    add translations

---
+++ i18n/base.ts
@@ -0,0 +1,247 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="de" sourcelanguage="en">
+  <context>
+    <name>Application</name>
+    <message>
+      <source>This program needs root privileges to run!</source>
+      <translation type="unfinished"></translation>
+    </message>
+  </context>
+  <context>
+    <name>MainWindow</name>
+    <message>
+      <source>brightness:</source>
+      <translation type="unfinished"></translation>
+    </message>
+    <message>
+      <source>Program</source>
+      <translation type="unfinished"></translation>
+    </message>
+    <message>
+      <source>Save configuration</source>
+      <translation type="unfinished"></translation>
+    </message>
+    <message>
+      <source>About Qt</source>
+      <translation type="unfinished"></translation>
+    </message>
+    <message>
+      <source>About ...</source>
+      <translation type="unfinished"></translation>
+    </message>
+    <message>
+      <source>Quit</source>
+      <translation type="unfinished"></translation>
+    </message>
+    <message>
+      <source>backlight is on</source>
+      <translation type="unfinished"></translation>
+    </message>
+    <message>
+      <source>backlight is off</source>
+      <translation type="unfinished"></translation>
+    </message>
+    <message>
+      <source>Mode:</source>
+      <translation type="unfinished"></translation>
+    </message>
+  </context>
+  <context>
+    <name>ColorFrame</name>
+    <message>
+      <source>single color</source>
+      <translation type="unfinished"></translation>
+    </message>
+    <message>
+      <source>multiple colors</source>
+      <translation type="unfinished"></translation>
+    </message>
+    <message>
+      <source>left color:</source>
+      <translation type="unfinished"></translation>
+    </message>
+    <message>
+      <source>center color:</source>
+      <translation type="unfinished"></translation>
+    </message>
+    <message>
+      <source>right color:</source>
+      <translation type="unfinished"></translation>
+    </message>
+    <message>
+      <source>extra color:</source>
+      <translation type="unfinished"></translation>
+    </message>
+    <message>
+      <source>color:</source>
+      <translation type="unfinished"></translation>
+    </message>
+  </context> 
+  <context>
+    <name>ModeListModel</name>
+    <message>
+      <source>Color</source>
+      <translation type="unfinished"></translation>
+    </message>
+    <message>
+      <source>Breathe</source>
+      <translation type="unfinished"></translation>
+    </message>
+    <message>
+      <source>Cycle</source>
+      <translation type="unfinished"></translation>
+    </message>
+    <message>
+      <source>Dance</source>
+      <translation type="unfinished"></translation>
+    </message>
+    <message>
+      <source>Flash</source>
+      <translation type="unfinished"></translation>
+    </message>
+    <message>
+      <source>Random Color</source>
+      <translation type="unfinished"></translation>
+    </message>
+    <message>
+      <source>Tempo</source>
+      <translation type="unfinished"></translation>
+    </message>
+    <message>
+      <source>Wave</source>
+      <translation type="unfinished"></translation>
+    </message>
+  </context>
+  <context>
+    <name>ColorListModel</name>
+    <message>
+      <source>black</source>
+      <translation type="unfinished"></translation>
+    </message>
+    <message>
+      <source>white</source>
+      <translation type="unfinished"></translation>
+    </message>
+    <message>
+      <source>gray</source>
+      <translation type="unfinished"></translation>
+    </message>
+    <message>
+      <source>silver</source>
+      <translation type="unfinished"></translation>
+    </message>
+    <message>
+      <source>red</source>
+      <translation type="unfinished"></translation>
+    </message>
+    <message>
+      <source>maroon</source>
+      <translation type="unfinished"></translation>
+    </message>
+    <message>
+      <source>orange</source>
+      <translation type="unfinished"></translation>
+    </message>
+    <message>
+      <source>orangered</source>
+      <translation type="unfinished"></translation>
+    </message>
+    <message>
+      <source>yellow</source>
+      <translation type="unfinished"></translation>
+    </message>
+    <message>
+      <source>olive</source>
+      <translation type="unfinished"></translation>
+    </message>
+    <message>
+      <source>lime</source>
+      <translation type="unfinished"></translation>
+    </message>
+    <message>
+      <source>green</source>
+      <translation type="unfinished"></translation>
+    </message>
+    <message>
+      <source>aqua</source>
+      <translation type="unfinished"></translation>
+    </message>
+    <message>
+      <source>teal</source>
+      <translation type="unfinished"></translation>
+    </message>
+    <message>
+      <source>blue</source>
+      <translation type="unfinished"></translation>
+    </message>
+    <message>
+      <source>navy</source>
+      <translation type="unfinished"></translation>
+    </message>
+    <message>
+      <source>fuchsia</source>
+      <translation type="unfinished"></translation>
+    </message>
+    <message>
+      <source>purple</source>
+      <translation type="unfinished"></translation>
+    </message>
+    <message>
+      <source>salmon</source>
+      <translation type="unfinished"></translation>
+    </message>
+    <message>
+      <source>slateblue</source>
+      <translation type="unfinished"></translation>
+    </message>
+    <message>
+      <source>lightgreen</source>
+      <translation type="unfinished"></translation>
+    </message>
+    <message>
+      <source>khaki</source>
+      <translation type="unfinished"></translation>
+    </message>
+  </context>
+  <context>
+    <name>ColorComboBox</name>
+    <message>
+      <source>more ...</source>
+      <translation type="unfinished"></translation>
+    </message>
+  </context>
+  <context>
+    <name>KBBacklightControl</name>
+    <message>
+      <source>Save Configuration</source>
+      <translation type="unfinished"></translation>
+    </message>
+    <message>
+      <source>Do you want to create a backup of the current configuration?</source>
+      <translation type="unfinished"></translation>
+    </message>
+  </context>
+  <context>
+    <name>About</name>
+    <message>
+      <source>About Qt</source>
+      <translation type="unfinished"></translation>
+    </message>
+    <message>
+      <source>About ...</source>
+      <translation type="unfinished"></translation>
+    </message>
+    <message>
+      <source>&lt;p&gt;&lt;h3&gt;About TUXEDO Keyboard Backlight Control&lt;/h3&gt;&lt;/p&gt;"
+       &lt;p&gt;This small tool takes the idea of the &lt;a href=\"https://github.com/webketje/tuxedo-backlight-control\"&gt;TUXEDO Backlight Control&lt;/a&gt; tool.
+          It currently provides only a graphical interface. But the advantage of this tool is the opportunity to store the user defined 
+          settings to the configuration file of the &lt;a href=\"[(https://github.com/tuxedocomputers/tuxedo-keyboard\"&gt;tuxedo-keyboard&lt;/a&gt; kernel module.&lt;br /&gt;
+          It does not claim to be completed or to provide and support a program for productive use.&lt;/p&gt;
+       &lt;p&gt;The source code is licensed under the BSD-3-Clause-License and published on
+       &lt;a href=\"https://github.com/pmfoss/tuxedo-keyboard-backlight-control\"&gt;Github&lt;/a&gt;&lt;/p&gt;
+       &lt;p&gt;Copyright (C) 2022 Martin Pietsch &lt;@pmfoss&gt;&lt;/p&gt;</source>
+      <translation type="unfinished"></translation>
+    </message>
+  </context>
+</TS>
---
+++ i18n/de.ts
@@ -0,0 +1,240 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="de" sourcelanguage="en">
+  <context>
+    <name>Application</name>
+    <message>
+      <source>This program needs root privileges to run!</source>
+      <translation>Diese Programm benötigt zur Ausführung Root-Rechte!</translation>
+    </message>
+  </context>
+  <context>
+    <name>MainWindow</name>
+    <message>
+      <source>brightness:</source>
+      <translation>Helligkeit:</translation>
+    </message>
+    <message>
+      <source>Program</source>
+      <translation>Programm</translation>
+    </message>
+    <message>
+      <source>Save configuration</source>
+      <translation>Konfiguration sichern</translation>
+    </message>
+    <message>
+      <source>About Qt</source>
+      <translation>Über Qt</translation>
+    </message>
+    <message>
+      <source>About ...</source>
+      <translation>Über ...</translation>
+    </message>
+    <message>
+      <source>Quit</source>
+      <translation>Beenden</translation>
+    </message>
+    <message>
+      <source>backlight is on</source>
+      <translation>Hintergrundbeleuchtung ist eingeschaltet</translation>
+    </message>
+    <message>
+      <source>backlight is off</source>
+      <translation>Hintergrundbeleuchtung ist ausgeschaltet</translation>
+    </message>
+    <message>
+      <source>Mode:</source>
+      <translation>Modus:</translation>
+    </message>
+  </context>
+  <context>
+    <name>ColorFrame</name>
+    <message>
+      <source>single color</source>
+      <translation>einzelne Farbe</translation>
+    </message>
+    <message>
+      <source>multiple colors</source>
+      <translation>mehrere Farben</translation>
+    </message>
+    <message>
+      <source>left color:</source>
+      <translation>linke Farbe:</translation>
+    </message>
+    <message>
+      <source>center color:</source>
+      <translation>mittlere Farbe:</translation>
+    </message>
+    <message>
+      <source>right color:</source>
+      <translation>rechte Farbe:</translation>
+    </message>
+    <message>
+      <source>extra color:</source>
+      <translation>Zusatzfarbe:</translation>
+    </message>
+    <message>
+      <source>color:</source>
+      <translation>Farbe:</translation>
+    </message>
+  </context> 
+  <context>
+    <name>ModeListModel</name>
+    <message>
+      <source>Color</source>
+      <translation>Farbe</translation>
+    </message>
+    <message>
+      <source>Breathe</source>
+      <translation>Atmen</translation>
+    </message>
+    <message>
+      <source>Cycle</source>
+      <translation>Zyklus</translation>
+    </message>
+    <message>
+      <source>Dance</source>
+      <translation>Tanz</translation>
+    </message>
+    <message>
+      <source>Flash</source>
+      <translation>Blitz</translation>
+    </message>
+    <message>
+      <source>Random Color</source>
+      <translation>Zufallsfarbe</translation>
+    </message>
+    <message>
+      <source>Tempo</source>
+      <translation>Tempo</translation>
+    </message>
+    <message>
+      <source>Wave</source>
+      <translation>Welle</translation>
+    </message>
+  </context>
+  <context>
+    <name>ColorListModel</name>
+    <message>
+      <source>black</source>
+      <translation>schwarz</translation>
+    </message>
+    <message>
+      <source>white</source>
+      <translation>weiss</translation>
+    </message>
+    <message>
+      <source>gray</source>
+      <translation>grau</translation>
+    </message>
+    <message>
+      <source>silver</source>
+      <translation>silber</translation>
+    </message>
+    <message>
+      <source>red</source>
+      <translation>rot</translation>
+    </message>
+    <message>
+      <source>maroon</source>
+      <translation>kastanienbraun</translation>
+    </message>
+    <message>
+      <source>orange</source>
+      <translation>orange</translation>
+    </message>
+    <message>
+      <source>orangered</source>
+      <translation>orangerot</translation>
+    </message>
+    <message>
+      <source>yellow</source>
+      <translation>gelb</translation>
+    </message>
+    <message>
+      <source>olive</source>
+      <translation>oliv</translation>
+    </message>
+    <message>
+      <source>lime</source>
+      <translation>limettengrün</translation>
+    </message>
+    <message>
+      <source>green</source>
+      <translation>grün</translation>
+    </message>
+    <message>
+      <source>aqua</source>
+      <translation>türkis</translation>
+    </message>
+    <message>
+      <source>teal</source>
+      <translation>aquamarin</translation>
+    </message>
+    <message>
+      <source>blue</source>
+      <translation>blau</translation>
+    </message>
+    <message>
+      <source>navy</source>
+      <translation>marineblau</translation>
+    </message>
+    <message>
+      <source>fuchsia</source>
+      <translation>fuchsienfarben</translation>
+    </message>
+    <message>
+      <source>purple</source>
+      <translation>lila</translation>
+    </message>
+    <message>
+      <source>salmon</source>
+      <translation>lachsrot</translation>
+    </message>
+    <message>
+      <source>slateblue</source>
+      <translation>schieferblau</translation>
+    </message>
+    <message>
+      <source>lightgreen</source>
+      <translation>hellgrün</translation>
+    </message>
+    <message>
+      <source>khaki</source>
+      <translation>khaki</translation>
+    </message>
+  </context>
+  <context>
+    <name>ColorComboBox</name>
+    <message>
+      <source>more ...</source>
+      <translation>mehr ...</translation>
+    </message>
+  </context>
+  <context>
+    <name>KBBacklightControl</name>
+    <message>
+      <source>Save Configuration</source>
+      <translation>Konfiguration sichern</translation>
+    </message>
+    <message>
+      <source>Do you want to create a backup of the current configuration?</source>
+      <translation>Möchten Sie ein Backup der aktuellen Konfiguration erstellen?</translation>
+    </message>
+  </context>
+  <context>
+    <name>About</name>
+    <message>
+      <source>About Qt</source>
+      <translation>Über Qt</translation>
+    </message>
+    <message>
+      <source>About ...</source>
+      <translation>Über ...</translation>
+    </message>
+    <message>
+      <source>&lt;p&gt;&lt;h3&gt;About TUXEDO Keyboard Backlight Control&lt;/h3&gt;&lt;/p&gt; &lt;p&gt;This small tool takes the idea of the &lt;a href=&quot;https://github.com/webketje/tuxedo-backlight-control&quot;&gt;TUXEDO Backlight Control&lt;/a&gt; tool. It currently provides only a graphical interface. But the advantage of this tool is the opportunity to store the user defined settings to the configuration file of the &lt;a href=&quot;https://github.com/tuxedocomputers/tuxedo-keyboard&quot;&gt;tuxedo-keyboard&lt;/a&gt; kernel module.&lt;br /&gt; The program does not claim to be completed or to provide and support a program for productive use.&lt;/p&gt; &lt;p&gt;The source code is licensed under the BSD-3-Clause-License and published on &lt;a href=&quot;https://github.com/pmfoss/tuxedo-keyboard-backlight-control&quot;&gt;Github&lt;/a&gt;&lt;/p&gt; &lt;p&gt;Copyright (C) 2022 Martin Pietsch (&#64;pmfoss)&lt;/p&gt;</source>
+      <translation>&lt;p&gt;&lt;h3&gt;Über TUXEDO Keyboard Backlight Control&lt;/h3&gt;&lt;/p&gt; &lt;p&gt;Dieses Tool übernimmt die Idee von &lt;a href=&quot;https://github.com/webketje/tuxedo-backlight-control&quot;&gt;TUXEDO Backlight Control.&lt;/a&gt; Es bietet aktuell nur eine grafische Oberfläche. Der Vorteil dieses Tools ist jedoch die Möglichkeit, die benutzerdefinierten Einstellungen in der Konfigurationsdatei des &lt;a href=&quot;https://github.com/tuxedocomputers/tuxedo-keyboard&quot;&gt;tuxedo-keyboard&lt;/a&gt; Kernel Modules zu speichern.&lt;br /&gt; Das Programm erhebt nicht den Anspruch, abgeschlossen zu sein oder ein Programm zur produktiven Nutzung bereitzustellen und zu unterstützen.&lt;/p&gt; &lt;p&gt;Der Quellcode ist unter der BSD-3-Clause-License lizenziert und auf &lt;a href=&quot;https://github.com/pmfoss/tuxedo-keyboard-backlight-control&quot;&gt;Github&lt;/a&gt; veröffentlicht.&lt;/p&gt; &lt;p&gt;Copyright (C) 2022 Martin Pietsch (&#64;pmfoss)&lt;/p&gt;</translation>
+    </message>
+  </context>
+</TS>
